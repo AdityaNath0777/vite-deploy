0fe85bf6709b3f46baf2c878ec3dcc79178f7d23
Aditya_Mishra <adityasaiyansuper@gmail.com>
2024-08-12 22:38:42 +0530

    fix(router): correct routing paths with base URL for GitHub Pages deployment

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
+  const basePath = "/vite-deploy"; // Define the base path
   return (
     <div className='bg-sky-50 w-full min-h-14  flex justify-evenly  items-center px-4 py-3'>
                       <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                           <li>
                               <NavLink
-                                  to = '/'
+                                  to ={`${basePath}/`} // Use base path
                                   className={({isActive}) =>
                                       `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent ${isActive ? "text-orange-600" : "text-gray-600" } lg:border-0 hover:text-orange-700 lg:p-0`
                                   }
@@ -17,7 +18,7 @@ const Navbar = () => {
                           </li>
                           <li>
                               <NavLink
-                                  to = '/about'
+                                  to = {`${basePath}/about`} // Use base path
                                   className={({isActive}) =>
                                       `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent ${isActive ? "text-orange-600" : "text-gray-600" } lg:border-0 hover:text-orange-700 lg:p-0`
                                   }
@@ -27,7 +28,7 @@ const Navbar = () => {
                           </li>
                           <li>
                               <NavLink
-                                  to = '/contact'
+                                  to = {`${basePath}/contact`} // Use base path
                                   className={({isActive}) =>
                                       `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent ${isActive ? "text-orange-600" : "text-gray-600" } lg:border-0 hover:text-orange-700 lg:p-0`
                                   }
--- src/main.jsx
+++ src/main.jsx
@@ -12,8 +12,8 @@ import { About, Contact, Home } from "./components";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      <Route path="" element={<Home />} />
+    <Route path="/vite-deploy/" element={<App />}>
+      <Route index element={<Home />} />
       <Route path="home" element={<Home />} />
       <Route path="about" element={<About />} />
       <Route path="contact" element={<Contact />} />
